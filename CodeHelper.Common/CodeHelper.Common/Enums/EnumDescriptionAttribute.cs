3701b6be346c5b394749446f1df1e3a06240d221
15868707168@163.com <15868707168@163.com>
2023-05-09 15:57:23 +0800

    更新到项目

---
+++ CodeHelper.Common/CodeHelper.Common/Enums/EnumDescriptionAttribute.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeHelper.Common
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class EnumDescriptionAttribute : Attribute
+    {
+        private string m_strDescription;
+
+        public EnumDescriptionAttribute(string strPrinterName)
+        {
+            m_strDescription = strPrinterName;
+        }
+
+        public string Description
+        {
+            get { return m_strDescription; }
+        }
+
+        public static string? GetEnumDescription(Enum enumObj)
+        {
+            System.Reflection.FieldInfo? fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
+
+            try
+            {
+                object[]? attribArray = fieldInfo?.GetCustomAttributes(false);
+                if (attribArray is not null)
+                {
+                    if (attribArray.Length == 0)
+                    {
+                        return String.Empty;
+                    }
+                    else
+                    {
+                        EnumDescriptionAttribute? attrib = attribArray[0] as EnumDescriptionAttribute;
+
+                        return attrib?.Description;
+                    }
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Utlity
-common help classes
+<h1 align="center"><p>🦓 CodeHelper.Common</p>
+</h1>
+
+<div align="center"> 
+<p> CodeHelper.Common 是一个封装常用帮助类的 Nuget 包 😼</p>
+</div>
+
+###  :zap: HTTP 响应状态码 
+
+> [MDN HTTP 项目状态码描述](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Status#%E4%BF%A1%E6%81%AF%E5%93%8D%E5%BA%94)
+
++ 枚举 **HttpStatus**
+
