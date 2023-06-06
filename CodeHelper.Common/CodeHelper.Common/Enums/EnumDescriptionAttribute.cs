bbbbc47b782804629ad104bfa53a7a6b426c68bf
15868707168@163.com <15868707168@163.com>
2023-06-06 17:18:56 +0800

    新增了通过枚举描述获取枚举值得方法

--- CodeHelper.Common/CodeHelper.Common/Enums/EnumDescriptionAttribute.cs
+++ CodeHelper.Common/CodeHelper.Common/Enums/EnumDescriptionAttribute.cs
@@ -15,6 +15,11 @@
             get { return m_strDescription; }
         }
 
+        /// <summary>
+        /// 获取枚举描述
+        /// </summary>
+        /// <param name="enumObj"></param>
+        /// <returns></returns>
         public static string? GetEnumDescription(Enum enumObj)
         {
             System.Reflection.FieldInfo? fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
@@ -41,6 +46,28 @@
             {
                 return null;
             }
+        }
+
+        /// <summary>
+        /// 通过【描述】获取【枚举值】
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static T? GetEnumByDescription<T>(string description) where T : Enum
+        {
+            System.Reflection.FieldInfo[] fields = typeof(T).GetFields();
+            foreach (System.Reflection.FieldInfo field in fields)
+            {
+                object[] objs = field.GetCustomAttributes(typeof(EnumDescriptionAttribute), false);    //获取描述属性
+                if (objs.Length > 0 && (objs[0] as EnumDescriptionAttribute)?.Description == description)
+                {
+                    return (T?)field.GetValue(null);
+                }
+            }
+
+            throw new ArgumentException(string.Format("{0} 未能找到对应的枚举.", description), "Description");
         }
     }
 }
